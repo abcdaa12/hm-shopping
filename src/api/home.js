ab155bf99b6ff34ea353b76ec478d2e4d98b38f9
yyr <2997342346@qq.com>
2025-02-22 19:54:08 +0800

    v2.1主页

--- src/api/home.js
+++ src/api/home.js
@@ -1,6 +1,7 @@
 import request from '@/utils/request'
 
 // 获取首页数据
+// 封装接口
 export const getHomeData = () => {
   return request.get('/page/detail', {
     params: {
--- src/components/GoodsItem.vue
+++ src/components/GoodsItem.vue
@@ -1,17 +1,16 @@
 <template>
-  <div class="goods-item" >
+  <div class="goods-item" v-if="item.goods_id" @click="$router.push(`/prodetail/${item.goods_id}`)">
     <div class="left">
-      <img src="@/assets/product.jpg" alt="" />
+      <img :src="item.goods_image" alt="" />
     </div>
     <div class="right">
       <p class="tit text-ellipsis-2">
-        三星手机 SAMSUNG Galaxy S23 8GB+256GB 超视觉夜拍系统 超清夜景 悠雾紫
-        5G手机 游戏拍照旗舰机s23
+        {{item.goods_name}}
       </p>
-      <p class="count">已售104件</p>
+      <p class="count">已售{{item.goods_sales}}件</p>
       <p class="price">
-        <span class="new">¥3999.00</span>
-        <span class="old">¥6699.00</span>
+        <span class="new">¥{{item.goods_price_min}}</span>
+        <span class="old">¥{{item.goods_price_max}}</span>
       </p>
     </div>
   </div>
@@ -20,7 +19,16 @@
 <script>
 
 export default {
-  name: 'GoodsItem'
+  name: 'GoodsItem',
+  props: {
+    item: {
+      type: Object,
+      // 对象类型的默认值是一个函数,函数的返回值就是默认值
+      default: () => {
+        return {}
+      }
+    }
+  }
 }
 </script>
 
