e838aa61ca350694f5981e9d0ff4e55902e247e8
yyr <2997342346@qq.com>
2025-02-22 17:29:54 +0800

    v1.2

--- src/main.js
+++ src/main.js
@@ -3,6 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import '@/utils/vant-ui'
+import '@/styles/common.less'
+Vue.config.productionTip = false
+// import { Toast } from 'vant'
+// Toast('嘿嘿，你好哇')
+// 全部导入
+// import Vant from 'vant'
+// import 'vant/lib/index.css'
+// // 插件安装初始化：内部会将所有的vant所有组件进行导入注册
+// Vue.use(Vant)
 new Vue({
   router,
   store,
--- src/utils/vant-ui.js
+++ src/utils/vant-ui.js
@@ -1,9 +1,17 @@
 // 按需导入vant
 import Vue from 'vue'
-import { Button, Switch, Rate, Tabbar, TabbarItem } from 'vant'
+import { Button, Switch, Rate, Tabbar, TabbarItem, Swipe, SwipeItem, Grid, GridItem, Search, Toast, NavBar } from 'vant'
+
+Vue.use(GridItem)
+Vue.use(Search)
+Vue.use(Swipe)
+Vue.use(SwipeItem)
+Vue.use(Grid)
 Vue.use(Button)
 Vue.use(Switch)
 Vue.use(Rate)
 Vue.use(Tabbar)
 Vue.use(TabbarItem)
+Vue.use(Toast)
+Vue.use(NavBar)
 Vue.config.productionTip = false
